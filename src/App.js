089c9eb5336526d0631be74d467bf8ffc8bc6b5f
Gabriel Anguiano <anguianog97@gmail.com>
2019-12-19 10:56:11 -0800

    gabe commit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
         </a>
         !
       </h2>
+      <h3>Test 3</h3>
       <p>
         <a
           href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
